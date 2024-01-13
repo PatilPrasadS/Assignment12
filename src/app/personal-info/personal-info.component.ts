6a0a6e6c42770da3afb344426e13d78129c90a82
PatilPrasadS <prsyy1234@gmail.com>
2024-01-13 08:22:15 +0530

    Bug Report

---
+++ src/app/personal-info/personal-info.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-personal-info',
+  standalone: true,
+  imports: [],
+  templateUrl: './personal-info.component.html',
+  styleUrl: './personal-info.component.css'
+})
+export class PersonalInfoComponent {
+  public fname = "";
+  public lname = "";
+  public email = "";
+
+  
+}
